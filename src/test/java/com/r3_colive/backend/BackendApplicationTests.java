b94848802bb9b025edd67b8cf65493afa87f4f66
sudhansu97ssp <patisudhansu1996@gmail.com>
2025-04-12 21:35:30 +0530

    Initial Commit

---
+++ src/test/java/com/r3_colive/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r3_colive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
